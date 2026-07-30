2dfa08686b566bd019ee01e0a8f8b032aef55cbd
Henry Carvalho <henrycarvalho2008@outlook.com>
2026-07-30 14:54:58 -0700

    Comleted

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,31 @@
 import { useState } from "react";
 import Canvas from "./Canvas";
-import RoutedApp from "./RoutedApp";
 
 function App() {
-
   const [digit, setDigit] = useState(null);
   const [confidence, setConfidence] = useState(null);
-
   return (
-    <RoutedApp />
-
+    <div className="container">
+      <h1>AI Digit Recognizer</h1>
+      <Canvas
+        setDigit={setDigit}
+        setConfidence={setConfidence}
+      />
+      <div className="result">
+        <h2>
+          Prediction
+        </h2>
+        <h1>
+          {digit ?? "-"}
+        </h1>
+        <p>
+          Confidence
+          <br />
+          {confidence ?? "-"}
+        </p>
+      </div>
+    </div>
   );
-
 }
 
 export default App;
